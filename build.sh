9aca114b5f7a8b39cee9c916524b6109d84b1cdc
dairh <342751099@qq.com>
2021-07-05 20:34:03 +0800

    1、sdk提交

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+yarn install
+npm run-script build
---
+++ src/theme/colors.ts
@@ -0,0 +1,55 @@
+import { Colors } from "./types";
+
+export const baseColors = {
+  failure: "#ED4B9E",
+  primary: "#AA2929",
+  primaryBright: "#AA2929",
+  primaryDark: "#AA2929",
+  secondary: "#AA2929",
+  success: "#56A923",
+  warning: "#FFB237",
+};
+
+export const brandColors = {
+  binance: "#F0B90B",
+};
+
+export const lightColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  background: "#FAF9FA",
+  backgroundDisabled: "#E9EAEB",
+  contrast: "#191326",
+  invertedContrast: "#FFFFFF",
+  input: "#eeeaf4",
+  tertiary: "#EFF4F5",
+  text: "#264B0F",
+  textDisabled: "#BDC2C4",
+  textSubtle: "#56A923",
+  borderColor: "#E9EAEB",
+  card: "#FFFFFF",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
+  },
+};
+
+export const darkColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  secondary: "#AA2929",
+  background: "#343135",
+  backgroundDisabled: "#3c3742",
+  contrast: "#FFFFFF",
+  invertedContrast: "#191326",
+  input: "#483f5a",
+  primaryDark: "#0098A1",
+  tertiary: "#353547",
+  text: "#EAEAEA",
+  textDisabled: "#666171",
+  textSubtle: "#DADADA",
+  borderColor: "#524B63",
+  card: "#27262c",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
+  },
+};
